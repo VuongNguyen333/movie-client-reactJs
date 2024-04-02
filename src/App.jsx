a99d12f17a999ad2b85d88c7dcc24ea9b927dbcc
vuongNguyenVg <vuongnguyen332003@gmail.com>
2024-04-02 23:59:22 +0700

    home-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
+import MainOrder from './pages/OrderPage/MainOrder'
 
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/order" element={<MainOrder />} />
     </Routes>
   )
 }
